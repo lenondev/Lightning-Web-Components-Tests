839b0dfa2ee1ababf61c4674017f210eeadc067b
Swordevcorp <91220033+Swordevcorp@users.noreply.github.com>
2022-07-20 20:21:16 +0200

    Update

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
-const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
+const { jestConfig } = require("@salesforce/sfdx-lwc-jest/config");
 
 module.exports = {
-    ...jestConfig,
-    modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
+  ...jestConfig,
+  moduleNameMapper: {
+    "^thunder/hammerButton$":
+      "<rootDir>/force-app/test/jest-mocks/thunder/hammerButton",
+    "^lightning/button$": "<rootDir>/force-app/test/jest-mocks/lightning/button"
+  },
+  modulePathIgnorePatterns: ["<rootDir>/.localdevserver"]
 };
